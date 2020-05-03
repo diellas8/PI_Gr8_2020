282b61d6175170f7e5361c5cd431997cb0e31202
Nora Berisha <nora.berisha@studentet.uni-pr.edu>
2020-05-03 21:37:38 +0200

    lidhja e php me mysql

--- src/php/mysql_conncect.php
+++ src/php/mysql_conncect.php
@@ -3,5 +3,21 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$database = "test12";
+
+
+$connection = mysqli_connect($servername, $username, $password, $database);
+
+if(!$connection)
+{
+    die("Error! Could not connect to database." . mysqli_connect_error());
+}
+else
+{
+    echo"Connected to database successfully.";
+}
+
+
+
 
 ?>
