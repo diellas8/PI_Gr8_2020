efdd2d340283df857bef30679ace60c0832d9fc5
Nora Berisha <nora.berisha@studentet.uni-pr.edu>
2020-05-03 22:46:43 +0200

    krahasimi i te dhenave me ato ne databaze dhe mbrojtja nga sql injections

--- src/php/login.php
+++ src/php/login.php
@@ -1,6 +1,46 @@
 <?php 
 require_once("mysql_conncect.php");
 
-echo"Miresevini ne faqen tone!"
+if (isset( $_POST["Submit"] ))
+{
+
+$username = $_POST["Username"];
+$password = $_POST["Password"];
+
+
+$username = mysqli_escape_string($connection, $username);
+$password = mysqli_escape_string($connection, $password);
+
+
+$errors = array();
+
+if(empty($username)){
+    array_push($errors, "Username is required");
+}
+if(empty($password)){
+    array_push($errors, "Password is required");
+}
+
+if(count($errors) == 0){
+    $password = md5($password);
+
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($connection, $query);
+    if(mysqli_num_rows($result) == 1){
+        echo"You are now logged in";
+    }
+    else {
+        echo "Wrong!";
+    }}
+else {
+    foreach($errors as $error){
+        echo $error . "\n";
+    }
+
+}
+}
+
+
+
 
 ?>
