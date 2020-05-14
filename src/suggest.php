58b6a18b810debad7e6205f7abf04167c1ee8e1a
endritbytyqi <58044635+endritbytyqi@users.noreply.github.com>
2020-05-15 01:47:35 +0200

    Ajax
    Perdorimi i ajax per lexim nga nje PHP skripte

---
+++ src/suggest.php
@@ -0,0 +1,43 @@
+<?php
+// People Array @TODO - Get from DB
+$people[] = "Nora";
+$people[] = "Diella";
+$people[] = "EndritB";
+$people[] = "EndritM";
+$people[] = "Vlera";
+$people[] = "Valon";
+$people[] = "Dardan";
+$people[] = "Blerim";
+$people[] = "Lule";
+$people[] = "Vjosa";
+$people[] = "Faton";
+$people[] = "Isak";
+$people[] = "Vigan";
+$people[] = "Edon";
+$people[] = "Valdete";
+$people[] = "Qefsere";
+$people[] = "Shqipe";
+$people[] = "Haxhi";
+$people[] = "Sevdie";
+
+// Get Query String
+$q = $_REQUEST['q'];
+
+$suggestion = "";
+
+// Get Suggestions
+if($q !== ""){
+	$q = strtolower($q);
+	$len = strlen($q);
+	foreach($people as $person){
+		if(stristr($q, substr($person, 0, $len))){
+			if($suggestion === ""){
+				$suggestion = $person;
+			} else {
+				$suggestion .= ", $person";
+			}
+		}
+	}
+}
+
+echo $suggestion === "" ? "No Suggestion" : $suggestion;
