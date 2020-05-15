498ede9dee101168bc06ea77e3e1f94ada8e9595
endritbytyqi <58044635+endritbytyqi@users.noreply.github.com>
2020-05-15 03:15:08 +0200

    Update search.php

--- src/search.php
+++ src/search.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Kerkesa 1 ajax</title>
-        <link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css">
-        <script>
-            function showSuggestion(str){
-                console.log(str);
-            }
-        </script>
-        <body>
-        <div class="container">
-            <h1>Kerko userat</h1>
-            <form>
-                Kerko: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
-            </form>
-        </div>
-
-        </body>
-
-    </head>
+<head>
+	<title>Ajax kerkesa 1</title>
+	<script>
+		function showSuggestion(str){
+			if(str.length == 0){
+				document.getElementById('output').innerHTML = '';
+			} else {
+				// AJAX REQ
+				var xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange = function(){
+					if(this.readyState == 4 && this.status == 200){
+						document.getElementById('output').innerHTML = this.responseText;
+					}
+				}
+				xmlhttp.open("GET", "suggest.php?q="+str, true);
+				xmlhttp.send();
+			}
+		}
+	</script>
+</head>
+<body>
+	<div class="container">
+	    <h1>Kerko userat</h1>
+	    <form> 
+	    	Kerko: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
+	    </form>
+	    <p>Sugjerime: <span id="output" style="font-weight:bold"></span></p>
+</div>
+</body>
 </html>
